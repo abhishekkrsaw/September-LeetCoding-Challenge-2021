27f37abcffea779af17e62fca510b40caabb7442
Abhishek Kumar Saw <78368340+abhishekkrsaw@users.noreply.github.com>
2021-09-13 23:19:53 +0530

    Update and rename Day 2 (Unique Binary Search Trees II) to Day 2 (Unique Binary Search Trees II).cpp

--- Day 2 (Unique Binary Search Trees II).cpp
+++ Day 2 (Unique Binary Search Trees II).cpp
@@ -1,3 +1,4 @@
+/*
 Question Link -> https://leetcode.com/problems/unique-binary-search-trees-ii/
 
 Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order. 
@@ -12,9 +13,10 @@ Output: [[1]]
 
 Constraints:
 1 <= n <= 8
+*/
 
 
-C++ Code:
+//C++ Code:
 
 class Solution {
 public:

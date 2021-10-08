01c74f900a88e523a8597ecc8d4400a563ef524e
Abhishek Kumar Saw <78368340+abhishekkrsaw@users.noreply.github.com>
2021-10-08 06:57:45 +0530

    Day 25 solution posted

--- Day 25 (Shortest Path in a Grid with Obstacles Elimination).cpp
+++ Day 25 (Shortest Path in a Grid with Obstacles Elimination).cpp
@@ -26,3 +26,45 @@ n == grid[i].length
 grid[i][j] is either 0 or 1.
 grid[0][0] == grid[m - 1][n - 1] == 0
 */
+
+//C++ Code:
+
+class Solution {
+public:
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<vector<int>> check(m,vector<int>(n,INT_MAX));
+        using temp = tuple<int,int,int>;
+        
+        queue<temp> q ;
+        q.push({0,0,0});
+        int dis = 0;
+        int dx[] = {0,0,-1,1};
+        int dy[] = {1,-1,0,0};
+        
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                auto [x,y,p] = q.front(); 
+                q.pop();
+                if(x==m-1 && y==n-1)
+                    return dis;
+                
+                for(int i = 0; i<4; i++){
+                    int nx = x+dx[i];
+                    int ny = y+dy[i];                    
+                    if(nx>=0 && nx<m && ny>=0 && ny<n){                        
+                        if(p+grid[nx][ny]>k || check[nx][ny]<=p+grid[nx][ny])
+                            continue;
+                        q.push({nx,ny, p+grid[nx][ny]});
+                        check[nx][ny] = p+grid[nx][ny];                        
+                    }
+                }                
+            }
+            dis++;
+        }        
+        return -1;
+    }
+};
